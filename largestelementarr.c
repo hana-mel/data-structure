566749660c2c7e14e7829194d1b6eb81a06acd88
Hanamel Risaw <hrisaw16@gmail.com>
2025-08-07 18:07:28 +0530

    largestelementarr.c

--- largestelementarr.c
+++ largestelementarr.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 void main()
 {
-    int n = 2,sum=0,i;
-    int a[n],max = a[0];
+    int sum=0,i;
+    int a[7],max = a[0];
     printf("Enter the elements of an array:");
-    for (i=0;i<n;i++)
+    for (i=0;i<7;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<7;i++)
     {
         sum = sum + a[i];
     }
     printf("The sum of elements of array - %d\n",sum);
-    for (i=0;i<n;i++)
+    for (i=0;i<7;i++)
     {
         if(max<a[i])
         {
