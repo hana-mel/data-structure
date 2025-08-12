e48f64f53bb62434f6606a2a5ab7d929729fad72
Hanamel Risaw <hrisaw16@gmail.com>
2025-08-12 18:51:54 +0530

    arraysum.c

--- arraysum.c
+++ arraysum.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 void main()
 {
-    int n = 7,sum=0,i;
+    int n,sum=0,i;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
     int a[n];
     printf("Enter the elements of an array:");
-    for (i=0;i<7;i++)
+    for (i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<7;i++)
+    for(i=0;i<n;i++)
     {
         sum = sum + a[i];
     }
     printf("The sum of elements of array - %d",sum);
 }
+
