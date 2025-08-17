71b87c00f456d9743f90de30781df979c75de464
Hanamel Risaw <hrisaw16@gmail.com>
2025-08-17 17:44:56 +0530

    bubblesortarr.c

--- bubblesortarr.c
+++ bubblesortarr.c
@@ -7,9 +7,9 @@ void main()
     {
         printf("%d \t",a[i]);
     }
-    for(i=0;i<6 ;i++)
+    for(i=0;i<6;i++)
     {
-        for(j=0;j<6 - i - 1;j++)
+        for(j=0;j<6-i-1;j++)
         {
             if(a[j] > a[j+1])
             {
