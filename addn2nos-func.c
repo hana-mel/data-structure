f01debde387f53e0e50ba7f0ef218f801572d787
Hanamel Risaw <hrisaw16@gmail.com>
2025-08-21 20:09:03 +0530

    addn2nos-func.c

--- addn2nos-func.c
+++ addn2nos-func.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int sum(int x,int y);
+int sum(int x,int y); //function declaration
 
 int main()
 {
-    int a,b,sum=0;
+    int a,b;
     printf("Enter any two numbers: ");
     scanf("%d %d",&a,&b);
-    printf("Sum = %d",a+b);
+    printf("Sum = %d",sum(a,b)); //function call
 }
 
-int sum(int x,int y)
+int sum(int x,int y) //function defination
 {
     return x + y;
 }
